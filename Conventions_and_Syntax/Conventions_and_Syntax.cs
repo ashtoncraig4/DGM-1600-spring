dc3a912a8510485a3886a37711a85849377455d9
ashtoncraig4 <ashton.craig4@gmail.com>
2017-02-02 18:54:45 -0700

    Syntax stuff

---
+++ Conventions_and_Syntax/Conventions_and_Syntax.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using System.Collections;
+
+public class BasicSyntax : MonoBehaviour
+{
+    void Start ()
+    }
+        Debug.Log(transform.position.x);
+
+        if(transform.position.x <= 5f)
+        {
+            Debug.Log ("I'm moving out!");
+        }
+    }
+}        
+
+public class BasicSyntax
+{
+    void Start ()
+    }
+        Debug.Log(transform.position.x);
+
+        if(transform.position.z <= 15f)
+        {
+            Debug.Log ("I'll go pick that up!");
+        }
+    }
+}
+
+public class BasicSyntax
+{
+    void Start ()
+    }
+        Debug.Log(transform.position.x);
+
+        if(transform.position.y <= 3f)
+        {
+            Debug.Log ("Impact!");
+        }
+    }
+}
+
+public class BasicSyntax
+{
+    void Start ()
+    }
+        Debug.Log(transform.position.x);
+
+        if(transform.position.z <= 12f)
+        {
+            Debug.Log ("Let me get that for you!");
+        }
+    }
+}
+
+public class BasicSyntax
+{
+    void Start ()
+    }
+        Debug.Log(transform.position.x);
+
+        if(transform.position.x <= 20f)
+        {
+            Debug.Log ("I'll attack that target!");
+        }
+    }
+}              
+              
