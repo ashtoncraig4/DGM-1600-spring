90e303cbf6734f69a76b67b744fa00a5087ac1c4
ashtoncraig4 <ashton.craig4@gmail.com>
2017-04-25 22:28:12 -0600

    Add files via upload

---
+++ Loops.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+public class WhileLoop : MonoBehaviour
+{
+    //The while loop can be thought of as a reapeating if statement.
+        int arrows > 6;
+
+        void Start ()
+        {
+            while (arrows) > 0)
+            {
+                Debug.Log ("I'm out of arrows!");
+                arrows--;
+            }
+        }
+}
+int mana > 100;
+
+        void Start ()
+        {
+            while (mana) > 0)
+            {
+                Debug.Log ("I'm out of mana!");
+                mana--;
+            }
+        }
+}
+int spells > 6;
+
+        void Start ()
+        {
+            while (spells) > 0)
+            {
+                Debug.Log ("I have no more spells!");
+                spells--;
+            }
+        }
+}
+
+    //Do while loop is a control flow statement that executes a block of code at least once then repeates the block or not.
+
+public class DoWhileLoop : MonoBehaviour
+{
+    void Start()
+    {
+        bool shouldContinue = false;
+
+        do
+        {
+            print ("Yolo!");
+
+        }while(shouldContine == true);
+    }
+}
+{
+    void Start()
+    {
+        bool shouldContinue = false;
+
+        do
+        {
+            print ("Hi World");
+
+        }while(shouldContine == true);
+    }
+}
+{
+    void Start()
+    {
+        bool shouldContinue = false;
+
+        do
+        {
+            print ("Tosh...Shhh");
+
+        }while(shouldContine == true);
+    }
+}
